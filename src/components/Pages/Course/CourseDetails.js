8e2539a69c48a39d637d860d3b2f0cadd47061fc
shohagroy <pkshohag240@gmail.com>
2022-10-26 03:04:08 +0600

    course details compoent mobile response added

--- src/components/Pages/Course/CourseDetails.js
+++ src/components/Pages/Course/CourseDetails.js
@@ -8,28 +8,30 @@ const CourseDetails = () => {
     courseDetails;
   return (
     <div>
-      <div className="w-100 mx-8 p-4 shadow-md bg-white text-gray-900">
+      <div className="w-100 mx-2 md:mx-8 p-4 shadow-md bg-white text-gray-900">
         <div className="flex justify-between pb-4 border-bottom">
           <div className="flex items-center">
-            <h3 className="text-2xl font-bold font-serif">{category}</h3>
+            <h3 className="text-lg md:text-2xl font-bold font-serif">
+              {category}
+            </h3>
           </div>
-          <p className="text-xl font-bold text-[#00CC83]">Price: {price}</p>
+          <p className=" md:text-xl font-bold text-[#00CC83]">Price: {price}</p>
         </div>
         <div className="space-y-4">
           <div className="space-y-2">
             <img
               src={img}
               alt={category}
-              className="block object-cover h-[80vh] object-center w-full rounded-md bg-gray-100"
+              className="block  md:h-[80vh] object-center w-full rounded-md bg-gray-100"
             />
-            <div className="flex items-center justify-between text-xl font-bold text-gray-600">
+            <div className="md:flex flex-col md:flex-row  md:items-center md:justify-between md:text-xl md:font-bold text-gray-600">
               <p>Course Duration: {duration}</p>
               <p>Pre-recorded video: {video}</p>
               <p>Live Class: {live}</p>
             </div>
           </div>
           <div className="space-y-2">
-            <h3 className="md:text-3xl font-bold text-black">{name}</h3>
+            <h3 className="text-3xl  font-bold text-black">{name}</h3>
             <p className="leading-snug text-gray-700">{details}</p>
           </div>
         </div>
