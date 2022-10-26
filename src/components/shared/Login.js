789c4ba534f2fabd44ae90b8217a0c71efd4f868
shohagroy <pkshohag240@gmail.com>
2022-10-27 02:15:21 +0600

    password show function code added

--- src/components/shared/Login.js
+++ src/components/shared/Login.js
@@ -23,6 +23,8 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const [viewPassword, setViewPassword] = useState(false);
+
   const path = location.state?.path?.pathname || "/";
 
   const userLoginHandelar = (e) => {
@@ -133,15 +135,59 @@ const Login = () => {
               className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-100 text-gray-900 focus:border-violet-400"
             />
           </div>
-          <div className="space-y-1 text-sm">
+          <div className="space-y-1 text-sm relative">
             <label className="block text-gray-400 text-left">Password</label>
             <input
-              type="password"
+              type={`${viewPassword ? "text" : "password"}`}
               name="password"
               id="password"
               placeholder="Password"
               className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-100 text-gray-900 focus:border-violet-400"
             />
+
+            <div
+              onClick={() => setViewPassword(!viewPassword)}
+              className="absolute top-[35%] cursor-pointer mr-2 text-gray-600 right-0"
+            >
+              <span className={`${viewPassword ? "hidden" : "visible"}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </span>
+
+              <span className={`${viewPassword ? "visible" : "hidden"}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              </span>
+            </div>
             <div className="flex cursor-pointer justify-end text-xs text-gray-400">
               <p onClick={() => setResetPassword(!resetPassword)}>
                 Forgot Password?
