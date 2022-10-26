0c06dd9d47ba4ff1b2c2da8fc6e2360434b1098b
shohagroy <pkshohag240@gmail.com>
2022-10-26 21:52:20 +0600

    login auth mobilel response added

--- src/components/shared/Navigation.js
+++ src/components/shared/Navigation.js
@@ -43,7 +43,7 @@ const Navigation = () => {
     <div className="absolute z-50   h-[10vh] w-full text-white bg-gray-100">
       <div
         onClick={() => setThemes(!themes)}
-        className="cursor-pointer absolute right-0 top-0 text-black p-3"
+        className="cursor-pointer absolute right-0 top-0 text-black md:p-3"
       >
         <span className={`${themes ? "hidden" : "block"}  duration-700 `}>
           <svg
@@ -315,7 +315,7 @@ const Navigation = () => {
           <div
             className={`${
               toggle ? "top-0" : "top-[-5000px]"
-            } absolute  left-0 duration-500 w-full h-[90vh] bg-gray-800/80`}
+            } absolute  left-0 duration-500 w-full h-[90vh] bg-[#0f256e]`}
           >
             <ul>
               <li
@@ -343,20 +343,57 @@ const Navigation = () => {
                 <NavLink to="blog">Blog</NavLink>
               </li>
             </ul>
-            <button className="w-full bg-blue-600 my-5 rounded-2xl text-white py-2">
-              Account
-            </button>
+            <Link to="/login" onClick={() => setToggle(!toggle)}>
+              <button className="w-full bg-[#00CC83] my-5 text-xl rounded-md text-white py-2">
+                Log in
+              </button>
+            </Link>
             <div className="text-center">
-              <p>
-                <small>Login with</small>
-              </p>
-              <span className="text-xl ">
-                <i className="fa-brands p-2 cursor-pointer hover:text-3xl duration-300 hover:text-orange-600 fa-facebook"></i>
-                <i className="fa-brands p-2 cursor-pointer hover:text-3xl duration-300 hover:text-orange-600 fa-twitter"></i>
-                <i className="fa-brands p-2 cursor-pointer hover:text-3xl duration-300 hover:text-orange-600 fa-google"></i>
-                <i className="fa-brands p-2 cursor-pointer hover:text-3xl duration-300 hover:text-orange-600 fa-github"></i>
-                <i className="fa-brands p-2 cursor-pointer hover:text-3xl duration-300 hover:text-orange-600 fa-linkedin"></i>
-              </span>
+              <div className="my-6 space-y-4">
+                <button
+                  aria-label="Login with Google"
+                  type="button"
+                  className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                    className="w-5 h-5 fill-current"
+                  >
+                    <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                  </svg>
+                  <p>Login with Google</p>
+                </button>
+                <button
+                  aria-label="Login with facebook"
+                  role="button"
+                  className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 32 32"
+                    className="w-5 h-5"
+                  >
+                    <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
+                  </svg>
+                  <p>Login with Facebook</p>
+                </button>
+                <button
+                  aria-label="Login with GitHub"
+                  role="button"
+                  className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                    className="w-5 h-5 fill-current"
+                  >
+                    <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                  </svg>
+                  <p>Login with GitHub</p>
+                </button>
+              </div>
             </div>
           </div>
         </div>
